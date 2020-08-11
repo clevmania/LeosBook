8d97f1b5594b26593efc08888ef946bf998f7541
clevmania <ernestcleverest@yahoo.com>
2020-08-11 05:39:16 +0100

    display intro slide on app launch

--- app/src/main/java/com/clevmania/leosbook/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/clevmania/leosbook/ui/splash/SplashFragment.kt
@@ -1,6 +1,5 @@
 package com.clevmania.leosbook.ui.splash
 
-import android.content.SharedPreferences
 import androidx.navigation.fragment.findNavController
 import com.clevmania.leosbook.R
 import com.clevmania.leosbook.ui.AuthFragment
@@ -11,18 +10,17 @@ import com.clevmania.leosbook.utils.InjectorUtils
  * @author by Lawrence on 8/1/20.
  * for LeosBook
  */
-class SplashFragment: AuthFragment() {
-    private  val sharedPreferences by lazy { InjectorUtils.provideSharedPreference(requireContext()) }
+class SplashFragment : AuthFragment() {
+    private val sharedPreferences by lazy { InjectorUtils.provideSharedPreference(requireContext()) }
 
     override fun onStart() {
         super.onStart()
-//        val boardingManager = OnBoardingManager(sharedPreferences)
-//        if (boardingManager.isFirstLaunch()) {
-//            boardingManager.setAppsFirstLaunch(false)
-//            findNavController().navigate(R.id.action_splashFragment_to_onBoardFragment)
-//        } else {
-//            findNavController().navigate(R.id.action_splashFragment_to_signInFragment)
-//        }
-        findNavController().navigate(R.id.action_splashFragment_to_onBoardFragment)
+        val boardingManager = OnBoardingManager(sharedPreferences)
+        if (boardingManager.isFirstLaunch()) {
+            boardingManager.setAppsFirstLaunch(false)
+            findNavController().navigate(R.id.action_splashFragment_to_onBoardFragment)
+        } else {
+            findNavController().navigate(R.id.action_splashFragment_to_signInFragment)
+        }
     }
 }
